5a02099c01e3f550af91f82a481f6aa9725b9605
yashika1705 <58276871+yashika1705@users.noreply.github.com>
2022-02-12 20:57:06 -0500

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,14 @@
-
+text = str(input("text\n"))
+string = str(input("string\n"))
+found = 0
+def shift(strin1, i=0):
+  return strin1[i::] + strin1[:i:]
+for s in range(len(string)):
+  final_str = shift(string, s)
+  if final_str in text:
+    found +=1
+    print("yes")
+    print(final_str)
+    break
+if found ==0:
+  print("no")
